dc034bc696ed641c2891671ab0637a37f636f6e3
rskanb <rs.kanbi@gmail.com>
2018-12-18 09:53:14 -0800

    Model Image

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,36 @@
 var friendArr = [
     {
         "name": "Jam",
-        "photo":"https://cors-anywhere.herokuapp.com/https://pixabay.com/en/wolf-lone-predator-wildlife-nature-3818343/",
+        "photo":"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
         "scores": [3,3,4,5,1,2,2,4,5,3]
     }
     ,
     {
         name: "Rax",
-        photo:"https://cors-anywhere.herokuapp.com/https://pixabay.com/en/wolf-lone-predator-wildlife-nature-3818343/",
+        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
         "scores": [5,5,3,3,1,1,2,2,4,4]
     },
     {
         name: "Bax",
-        photo:"https://cors-anywhere.herokuapp.com/https://pixabay.com/en/wolf-lone-predator-wildlife-nature-3818343/",
+        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
         "scores": [1,1,2,2,3,3,4,4,5,5]
     },{
         name: "Aago",
-        photo:"https://cors-anywhere.herokuapp.com/https://pixabay.com/en/wolf-lone-predator-wildlife-nature-3818343/",
+        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
         "scores": [1,2,3,3,4,5,4,1,1,3]
     },{
         name: "Chico",
-        photo:"https://cors-anywhere.herokuapp.com/https://pixabay.com/en/wolf-lone-predator-wildlife-nature-3818343/",
+        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
         "scores": [1,2,3,3,4,5,5,4,2,1]
     },
     {
         name: "Tino",
-        photo:"https://cors-anywhere.herokuapp.com/https://pixabay.com/en/wolf-lone-predator-wildlife-nature-3818343/",
+        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
         "scores": [1,5,4,3,1,2,1,3,4,5]
     },
     {
         name: "Muno",
-        photo:"https://cors-anywhere.herokuapp.com/https://pixabay.com/en/wolf-lone-predator-wildlife-nature-3818343/",
+        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
         "scores": [3,4,5,2,3,5,4,5,2,1]
     }
     ];
