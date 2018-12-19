ccda48760b93907c9cf18993c3a9a35d22a0bd08
rskanb <rs.kanbi@gmail.com>
2018-12-18 17:26:53 -0800

    Pic Linked From Pexel.com

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,36 @@
 var friendArr = [
     {
         "name": "Jam",
-        "photo":"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
+        "photo":"https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
         "scores": [3,3,4,5,1,2,2,4,5,3]
     }
     ,
     {
         name: "Rax",
-        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
+        photo: "https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
         "scores": [5,5,3,3,1,1,2,2,4,4]
     },
     {
         name: "Bax",
-        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
+        photo:"https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
         "scores": [1,1,2,2,3,3,4,4,5,5]
     },{
         name: "Aago",
-        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
+        photo:"https://images.pexels.com/photos/50582/selfie-monkey-self-portrait-macaca-nigra-50582.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
         "scores": [1,2,3,3,4,5,4,1,1,3]
     },{
         name: "Chico",
-        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
+        photo:"https://images.pexels.com/photos/162140/duckling-birds-yellow-fluffy-162140.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
         "scores": [1,2,3,3,4,5,5,4,2,1]
     },
     {
         name: "Tino",
-        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
+        photo:"https://images.pexels.com/photos/39571/gorilla-silverback-animal-silvery-grey-39571.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
         "scores": [1,5,4,3,1,2,1,3,4,5]
     },
     {
         name: "Muno",
-        photo:"https://cdn.pixabay.com/photo/2018/11/15/22/52/wolf-3818343_1280.jpg",
+        photo:"https://images.pexels.com/photos/257558/pexels-photo-257558.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
         "scores": [3,4,5,2,3,5,4,5,2,1]
     }
     ];
