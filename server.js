ca43818182a6e11772fc330f0f281d6d16b8db0d
rskanb <rs.kanbi@gmail.com>
2019-01-07 16:03:55 -0800

    Heroku Deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require("express");
 var app = express();
 
 //process.env.PORT ||
-var PORT =  8080;
+var PORT =  process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
